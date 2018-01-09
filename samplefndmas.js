36c74bedf3cb1ae2ae080b8aa731781e33539eaf
Gaya3vk <34084755+Gaya3vk@users.noreply.github.com>
2018-01-09 13:07:01 +0000

    Add files via upload

---
+++ samplefndmas.js
@@ -0,0 +1,26 @@
+//working with scopes
+var y=10;
+function name(){
+	y=20;
+
+}
+
+console.log(y);
+
+//working with loops
+a=5;
+while(true){
+ if(a>=10){
+break;}
+console.log(a);
+a=a+1;
+
+// Working with functions
+function foo(b){
+    a=a*2;
+    a=a+b;
+} a=10;
+foo(5);
+foo(25);
+foo(100);
+console.log(a);
